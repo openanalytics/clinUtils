087b14293170f6f08b01b3953e433ebce1af17cb
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-04-02 14:59:56 +0200

    remove import GLPG data in tests

--- package/clinUtils/tests/testthat.R
+++ package/clinUtils/tests/testthat.R
@@ -1,12 +1,5 @@
 library(testthat)
-library(glpgUtilityFct)
-
-# load data required for tests in the package
-data(ADaMDataPelican)
-data(labelVarsADaMPelican)
-
-dataAll <- ADaMDataPelican
-labelVars <- labelVarsADaMPelican
+library(clinUtils)
 
 if (Sys.getenv("TESTTHAT_OUTPUT_FILE") != "")
 	options(testthat.output_file = Sys.getenv("TESTTHAT_OUTPUT_FILE", stdout()))
