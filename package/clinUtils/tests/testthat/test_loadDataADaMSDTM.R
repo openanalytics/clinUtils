b489475d9903542adee8d381bdcd8934264877f3
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-04-02 14:07:06 +0200

    test loadDataADaMSDTM: remove glpgUtilityFct dep

--- package/clinUtils/tests/testthat/test_loadDataADaMSDTM.R
+++ package/clinUtils/tests/testthat/test_loadDataADaMSDTM.R
@@ -1,18 +1,25 @@
 context("Import SAS and xpt datasets")
 
 library(tools)
-
-pathSASDatasetsFiles <- list.files(path = system.file("extdata", package = "glpgUtilityFct"), pattern = "*.sas7bdat", full.names = TRUE)
+library(haven)
 
 test_that("Load data from SAS dataset files properly", {
 	
-	expect_message(adamData <- loadDataADaMSDTM(files = pathSASDatasetsFiles, verbose = TRUE))
+	# create dummy datasets
+	data <- data.frame(
+		USUBJID = sample.int(5),
+		TRT = c("A", "B", "A", "B", "A")
+	)
+	pathSAS7bdat <- tempfile(fileext = ".sas7bdat")
+	haven::write_sas(data = data, path = pathSAS7bdat)
+
+	expect_message(adamData <- loadDataADaMSDTM(files = pathSAS7bdat, verbose = TRUE))
 	listNames <- names(adamData)
 			
 	# Output expectations #
 	expect_is(adamData, "list")
 	expect_named(adamData)
-	fileName <- tools::file_path_sans_ext(basename(pathSASDatasetsFiles))
+	fileName <- tools::file_path_sans_ext(basename(pathSAS7bdat))
 	isFileImported <- sapply(fileName, function(name) any(grepl(name, x = names(adamData), ignore.case = TRUE)))
 	expect_true(all(isFileImported))
 	
@@ -24,53 +31,79 @@ test_that("Load data from SAS dataset files properly", {
 	
 	# Internal expectations #
 	expect_warning(
-		loadDataADaMSDTM(files = c(pathSASDatasetsFiles[1], pathSASDatasetsFiles[1])),
+		loadDataADaMSDTM(files = c(pathSAS7bdat, pathSAS7bdat)),
 		regexp = "duplicated file name"
 	)
 	expect_error(loadDataADamSDTM("test.xlsx"))
 	
 	# test 'encoding'
-	expect_error(loadDataADaMSDTM(files = pathSASDatasetsFiles, encoding = "test"))
+	expect_error(loadDataADaMSDTM(files = pathSAS7bdat, encoding = "test"))
 	
 	# pass arguments to 'read_sas'
-	dataSubset <- loadDataADaMSDTM(files = pathSASDatasetsFiles[[1]], n_max = 3)
+	dataSubset <- loadDataADaMSDTM(files = pathSAS7bdat, n_max = 3)
 	expect_equal(nrow(dataSubset[[1]]), 3)
 			
 })
 
 test_that("Load label variables from SAS dataset files properly", {
+			
+	# create dummy dataset
+	data <- data.frame(
+		USUBJID = structure(sample.int(5), label = "Subject ID"),
+		TRT = structure(c("A", "B", "A", "B", "A"), label = "Treatment"),
+		stringsAsFactors = FALSE
+	)
+	pathSAS7bdat <- tempfile(fileext = ".sas7bdat")
+	haven::write_sas(data = data, path = pathSAS7bdat)		
 		
-	adamData <- loadDataADaMSDTM(files = pathSASDatasetsFiles)	
+	adamData <- loadDataADaMSDTM(files = pathSAS7bdat)	
 	labelVars <- attr(adamData, "labelVars")
 	
 	expect_is(labelVars, "character")
 	expect_named(labelVars)
 	# all variables have labels, and all labels are present in the data:
-	expect_setequal(names(labelVars), unlist(lapply(adamData, names)))
+	expect_setequal(setdiff(names(labelVars), "DATASET"), colnames(data))
 			
 })
 
 test_that("Convert date variables properly", {
 			
-	pathData <- grep("adcm", pathSASDatasetsFiles, value = TRUE)
-	patternDate <- c("(DT|DTC)$")
+	# create dummy dataset
+	data <- data.frame(
+		USUBJID = sample.int(3), 
+		ADTC = c("2020-04-10", "2020-01-01", "2019-12-03"), 
+		stringsAsFactors = FALSE
+	)
+	pathData <- tempfile(fileext = ".sas7bdat")
+	haven::write_sas(data = data, path = pathData)				
 	
 	# variable in correct date format
-	correctDateVar <- "TRTSDT"
 	expect_message(
-		adamData <- loadDataADaMSDTM(files = pathData, 
-			convertToDate = TRUE, dateVars = patternDate),
-		message = paste("Convert .*", correctDateVar, "to calendar date/time format")
+		adamData <- loadDataADaMSDTM(
+			files = pathData, 
+			convertToDate = TRUE, dateVars = "ADTC"
+		),
+		message = paste("Convert date to calendar date/time format")
 	)
-	expect_is(adamData[["ADCM"]][[correctDateVar]], "POSIXct")
+	expect_is(adamData[[1]]$ADTC, "POSIXct")
 	
 	wrongDateFmtVar <- "CMSTDTC"
+	
+	# create dummy dataset
+	data <- data.frame(
+		USUBJID = sample.int(3), 
+		ADTC = c("20200410", "20200101", "20180309"), 
+		stringsAsFactors = FALSE
+	)
+	pathData <- tempfile(fileext = ".sas7bdat")
+	haven::write_sas(data = data, path = pathData)	
+	
 	expect_warning(
 		adamData <- loadDataADaMSDTM(files = pathData, 
-			convertToDate = TRUE, dateVars = patternDate),
-		message = paste(wrongDateFmtVar, "not of specified calendar date format")
+			convertToDate = TRUE, dateVars = "ADTC"),
+		message = "Date is not of specified calendar date format"
 	)
-	expect_is(adamData[["ADCM"]][[wrongDateFmtVar]], "character")
+	expect_is(adamData[[1]]$ADTC, "character")
 			
 })
 
@@ -81,12 +114,19 @@ test_that("Conversion to date/time", {
 	
 	expect_error(convertToDateTime(c(1, 2)))
 	
-	
 })
 
 test_that("Load data from xpt files properly", {
 			
-	pathXPTFile <- list.files(path = system.file("extdata", package = "glpgUtilityFct"), pattern = "*.xpt", full.names = TRUE)
+	# create dummy dataset
+	data <- data.frame(
+		USUBJID = structure(sample.int(5), label = "Subject ID"),
+		TRT = structure(c("A", "B", "A", "B", "A"), label = "Treatment"),
+		stringsAsFactors = FALSE
+	)
+	pathXPTFile <- tempfile(fileext = ".xpt")
+	haven::write_xpt(data = data, path = pathXPTFile)		
+			
 	expect_message(adamDataFromXpt <- loadDataADaMSDTM(files = pathXPTFile, verbose = TRUE))
 	
 	expect_is(adamDataFromXpt, "list")
