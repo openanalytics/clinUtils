f7eea6c58f5a3034562500a2d5369c9bef6cf6d9
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-04-02 15:00:47 +0200

    test check clinUtils

--- package/clinUtils/tests/testthat.R
+++ package/clinUtils/tests/testthat.R
@@ -4,6 +4,6 @@ library(clinUtils)
 if (Sys.getenv("TESTTHAT_OUTPUT_FILE") != "")
 	options(testthat.output_file = Sys.getenv("TESTTHAT_OUTPUT_FILE", stdout()))
 test_check(
-	"glpgUtilityFct",
+	"clinUtils",
 	reporter = Sys.getenv("TESTTHAT_DEFAULT_CHECK_REPORTER", "check")
 )
