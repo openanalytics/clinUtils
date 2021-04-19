4ccc57c5ec7c1842d9e2c14094b434decae2c5e1
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-04-19 11:19:00 +0200

    cleaner test_check

--- package/clinUtils/tests/testthat.R
+++ package/clinUtils/tests/testthat.R
@@ -1,9 +1,4 @@
 library(testthat)
 library(clinUtils)
 
-if (Sys.getenv("TESTTHAT_OUTPUT_FILE") != "")
-	options(testthat.output_file = Sys.getenv("TESTTHAT_OUTPUT_FILE", stdout()))
-test_check(
-	"clinUtils",
-	reporter = Sys.getenv("TESTTHAT_DEFAULT_CHECK_REPORTER", "check")
-)
+test_check("clinUtils")
