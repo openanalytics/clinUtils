4c08f7a04bd7a12502ff50180314178cc6bab13e
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-04-19 18:12:18 +0200

    example ADAM: use cdisc extended dataset + add cm, efficacy, and pp datasets

--- package/clinUtils/data_raw/createExampleDataset.R
+++ package/clinUtils/data_raw/createExampleDataset.R
@@ -1,7 +1,7 @@
 # Create example dataset for the package
 #
 # A subset of the CDISC Pilot 01 SDTM dataset, available from:
-# https://github.com/phuse-org/phuse-scripts/tree/master/data/[sdtm|adam]/cdiscpilot01/
+# https://github.com/phuse-org/phuse-scripts/tree/master/data/[sdtm|adam]/cdisc/
 # is exported for demonstration purpose
 #
 # Author: Laure Cougnaud
@@ -43,13 +43,22 @@ save(dataSDTMCDISCP01, version = 2, file = "../data/dataSDTMCDISCP01.RData")
 
 # Subset of the ADaM Datasets
 
-adamPath <- "~/git/phuse-scripts/data/adam/cdiscpilot01"
+adamPath <- "~/git/phuse-scripts/data/adam/cdisc"
 
-dataADaMPath <- list.files(pattern = "^(adsl|adae|adlbc|advs).xpt$", path = adamPath, full.names = TRUE)
+dataADaMPath <- list.files(
+	pattern = "^(adsl|adae|adlbc|advs|adcm|adqsadas|adqscibc|adpp).xpt$", 
+	path = adamPath, full.names = TRUE
+)
 dataADaMCDISCP01 <- loadDataADaMSDTM(files = dataADaMPath)
 labelVars <- attr(dataADaMCDISCP01, "labelVars")
-dataADaMCDISCP01 <- sapply(dataADaMCDISCP01, function(data){
-	dataSubset <- subset(data, USUBJID %in% subjects)
+subjectsPP <- c("Study001_1001001", "Study001_1001002", "Study001_1001003", "Study001_1001004" )
+dataADaMCDISCP01 <- sapply(names(dataADaMCDISCP01), function(dataset){
+	data <- dataADaMCDISCP01[[dataset]]
+	if(dataset == "ADPP"){
+		dataSubset <- subset(data, USUBJID %in% subjectsPP)
+	}else{
+		dataSubset <- subset(data, USUBJID %in% subjects)
+	}
 	rownames(dataSubset) <- NULL
 	dataSubset
 }, simplify = FALSE)
